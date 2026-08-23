41c4be8fa0b11f84105f3fee8709d921bfbcab5c
huongnv <huongvn.tdh@gmail.com>
2026-08-23 10:17:39 +0700

    Remove incompatible onError override - SDK 12.1.0 compatibility fix

--- app/src/main/java/com/example/myapplication/PlayerActivity.kt
+++ app/src/main/java/com/example/myapplication/PlayerActivity.kt
@@ -1,11 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class PlayerActivity : AppCompatActivity() {
@@ -23,12 +21,6 @@ class PlayerActivity : AppCompatActivity() {
         youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 youTubePlayer.loadVideo(videoId, 0f)
-            }
-
-            override fun onError(youTubePlayer: YouTubePlayer, errorReason: PlayerConstants.PlayerError?) {
-                super.onError(youTubePlayer, errorReason)
-                val code = errorReason?.ordinal ?: -1
-                Log.e("YT_PLAYER_ERROR", "YouTube Error Code: $code, Reason: $errorReason")
             }
         })
     }
